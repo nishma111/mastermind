f3b1819acb74df39684025a49446ef810ff31bd1
Nishma <nismaluitel@gmail.com>
2020-06-12 19:57:16 -0500

    Random number generated

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,27 @@
 import React from "react";
+import "./App.css";
 
-// Put code here
+//Put code here
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 
 function Game() {
-  return <div>Main Game</div>;
+  return (
+    <div className="App-header">
+      Main Menu
+      <div>
+        <button>Enter </button>
+        <div className="App">
+          {getRandomInt(0, 9)} {getRandomInt(0, 9)} {getRandomInt(0, 9)}{" "}
+          {getRandomInt(0, 9)}{" "}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Game;
