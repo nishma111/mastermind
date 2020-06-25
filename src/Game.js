d61e8be828741bb5d043d6d8a16d45b0e84f2974
sushmikc <sushmitakhadka98@gmail.com>
2020-06-25 13:14:35 -0500

    allows multiple attempt to user until correct answer

--- src/Game.js
+++ src/Game.js
@@ -1,23 +1,25 @@
 import React from "react";
 import "./App.css";
 
+// Put code here
+var solution= getRandomAnswer();
 class NameForm extends React.Component {
   constructor(props) {
     super(props);
+    this.initialState = {
+      value: ''};
     this.state = {value: ''};
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.checkAnswer = this.checkAnswer.bind(this);
   }
 
   handleChange(event) {
     this.setState({value: event.target.value});
   }
 
-  handleSubmit(event) {
-    event.preventDefault();
-    var solution= getRandomAnswer();
-    var solArray=solution.split("");
+  checkAnswer(solArray){
     var inputArray= this.state.value.split("");
 
     //variables to check for how many correct 
@@ -25,7 +27,7 @@ class NameForm extends React.Component {
     var colorCorrect= 0; //if only number is correct
                          //but needs to fix position
     var incorrect= 0; //if number and position both incorrect
-      
+    
       for(var i=0;i<solArray.length; i++)
       {
         for(var j=0; j<inputArray.length; j++)
@@ -46,30 +48,45 @@ class NameForm extends React.Component {
       if(correct===4)
       {
         alert('Correct answer! ');
+        console.log('Correct answer!');
+        this.setState(this.initialState);
+        //refreshPage();
+
       }
       else
       {
-        alert('Try Again! \n'+ 'Correct: '+ correct+ '\n Color Correct: '
+        console.log('Try Again! \n'+ 'Correct: '+ correct+ '\n Color Correct: '
         +colorCorrect+ '\n Incorrect: '+incorrect );
+        this.setState(this.initialState);
       }
-     
-    
   }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    var solArray=solution.split("");
+    this.checkAnswer(solArray);
+    
+    }
+
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <form id="input-form" onSubmit={this.handleSubmit}>
         <label>
           Guess:   
           <input type="text" value={this.state.value} onChange={this.handleChange} />
         </label>
-        <input type="submit" value="Submit" />
+        <input className="Submit" type="submit" value="Finish" />
       </form>
     );
   }
 }
 
-// Put code here
+//function to reload the page when input is correct
+function refreshPage() {
+  window.location.reload(false);
+}
+
 //function to generate a random number between [0-5]
 function getRandom(max)
 {
@@ -113,22 +130,22 @@ function getRandomAnswer()
 
 
 function Game() {
+  
   return (
     <div className="App-header">
       Mastermind
-      <div className="Output">
-        {getRandomAnswer()}
+      <div className="chances">
+        {solution}
+        <NameForm/>
       </div>
-    
-    <NameForm/>
 
       <div className="OptionPosition">
-        <button className="Options">0</button>
-        <button className="Options">1</button>
-        <button className="Options">2</button>
-        <button className="Options">3</button>
-        <button className="Options">4</button>
-        <button className="Options">5</button>
+        <button className="Options1">0</button>
+        <button className="Options2">1</button>
+        <button className="Options3">2</button>
+        <button className="Options4">3</button>
+        <button className="Options5">4</button>
+        <button className="Options6">5</button>
       </div>
     </div>
     
